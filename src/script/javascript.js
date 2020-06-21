90b1928f697f20bc614715acc4b474c9cb27d1e8
uxtothemax <romy.wiedijk@hva.nl>
2020-06-21 02:04:04 +0200

    Update script

---
+++ src/script/javascript.js
@@ -0,0 +1,14 @@
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#myCarousel").carousel("prev");
+});
+
+// https://www.w3schools.com/bootstrap/bootstrap_ref_js_carousel.asp
